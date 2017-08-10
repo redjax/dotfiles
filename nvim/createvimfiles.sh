6a12b57e12b8fe8bab67145eaadee59ed8824bdf
Jack Kenyon <jackenyon@gmail.com>
2017-08-10 13:46:40 -0400

    Converted VIM install script ot NVIM
    Modified VIM install script to install nvim and plugins instead of vim.

--- nvim/createvimfiles.sh
+++ nvim/createvimfiles.sh
@@ -4,11 +4,18 @@
 git clone git://github.com/redjax/dotfiles ~/dotfiles
 
 # Next, we copy the vimrc and vim folder from the dotfiles folder
-cp dotfiles/vim/.vimrc ~/.config/nvim/init.vim
-cp -R dotfiles/vim/.vim ~/.config/nvim/
+mkdir ~/.config/nvim
+cp dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+# Copy extra folders
+cp -R dotfiles/nvim/bundle ~/.config/nvim/
+cp -R dotfiles/nvim/plugged ~/.config/nvim/plugged
 
 # Then, we'll install Vundle before anything else
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 
 #Then, we'll run the VIM command to install all the Vundle plugins
-yes | nvim +PluginInstall +qall
+# yes | nvim +PluginInstall +qall
+
+chmod u+x vundleupdate.sh
+./vundleupdate.sh
