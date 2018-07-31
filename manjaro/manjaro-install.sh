49a1e027fa70a308ec412043a4bb0ac1738dafdf
Jack <jackenyon@gmail.com>
2018-07-31 15:36:41 -0400

    Remove whitespace in script.

--- manjaro/manjaro-install.sh
+++ manjaro/manjaro-install.sh
@@ -17,17 +17,17 @@ chown -R $usern /usr/share
 pacman-db-upgrade --noconfirm && pacman-optimize --noconfirm && sync
 
 # Install multimedia codecs
-pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca 
-libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 
-xvidcore gstreamer0.10-plugins flashplugin libdvdcss libdvdread libdvdnav 
+pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca
+libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264
+xvidcore gstreamer0.10-plugins flashplugin libdvdcss libdvdread libdvdnav
 gecko-mediaplayer dvd+rw-tools dvdauthor dvgrab --noconfirm
 
 # File utilities
 pacman -S file-roller seahorse-nautilus nautilus-share zlib unzip zip
 
 # Printer utilities
-pacman -S lib32-libcups cups gutenprint libpaper foomatic-db-engine 
-ghostscript gsfonts foomatic-db foomatic-filters cups-pdf 
+pacman -S lib32-libcups cups gutenprint libpaper foomatic-db-engine
+ghostscript gsfonts foomatic-db foomatic-filters cups-pdf
 system-config-printer --noconfirm
 
     # Enable printer utils
@@ -37,7 +37,7 @@ systemctl start org.cups.cupsd.service
 systemctl start cups-browsed.service
 
 # Enable numlock at startup
-gsettings set org.gnome.settings-daemon.peripherals.keyboard numlock-state 
+gsettings set org.gnome.settings-daemon.peripherals.keyboard numlock-state
 on
 
 # Install preload to speed up performance
