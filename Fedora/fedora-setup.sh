4d82bf1677cb2e23364af9f8c783796e0885c35b
Jack <jackenyon@gmail.com>
2019-02-05 10:41:17 -0500

    Tilix:
    Added Tilix install

--- Fedora/fedora-setup.sh
+++ Fedora/fedora-setup.sh
@@ -75,6 +75,28 @@ dnf install -y terminator
 
 # --------------------------------------------------------------
 
+# Install tilix
+dnf install -y tilix
+
+# --------------------------------------------------------------
+
+# Install TLP, laptop battery saving
+dnf install -y tlp tlp-rdw
+
+  # Thinkpad-specific
+  dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+  dnf install http://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
+
+  # Optional
+
+    # akmod-tp_smapi (battery charge threshold, recalibration
+    # akmod-acpi_call (X220/T420, X230/T430, etc)
+    # kernel-devel (needed for akmod packages)
+    dnf install -y akmod-tp_smapi akmod-acpi_call kernel-devel
+
+# --------------------------------------------------------------
+
 # Git install
 dnf install -y git
 
