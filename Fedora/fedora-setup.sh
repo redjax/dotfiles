5d620e4af8d2620e0ca3e9a7883d69b3494b7a28
Jack <jackenyon@gmail.com>
2019-02-05 10:56:44 -0500

    Brave Beta:
    Commented out Brave Dev, added Brave Beta

--- Fedora/fedora-setup.sh
+++ Fedora/fedora-setup.sh
@@ -18,9 +18,15 @@ sh -c 'curl https://www.folkswithhats.org/installer | bash'
 # -------------------------------------------------------------
 
 # Install Brave Browser Dev
-dnf config-manager --add-repo https://brave-browser-rpm-dev.s3.brave.com/x86_64/
-rpm --import https://brave-browser-rpm-dev.s3.brave.com/brave-core-nightly.asc
-dnf install -y brave-browser-dev
+# dnf config-manager --add-repo https://brave-browser-rpm-dev.s3.brave.com/x86_64/
+# rpm --import https://brave-browser-rpm-dev.s3.brave.com/brave-core-nightly.asc
+# dnf install -y brave-browser-dev
+
+# Brave Browser Beta
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+dnf install brave-browser brave-keyring
+
 # ------------------------------------------------------------
 
 # Install Sublime Text
