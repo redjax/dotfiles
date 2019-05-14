eec2de434f315e97eb22772b91fa081de2cc6f43
redjax <jackenyon@gmail.com>
2019-05-14 16:29:10 -0400

    Ubuntu scripts:
    Added some Ubuntu scripts & created a script for installing nvim on Ubuntu

---
+++ ubuntu/ubuntu_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup script for Ubuntu installs
+
+# Install snap store
+snap install snap-store
+
+# Install snaps
+snap install gimp
+snap install youtube-dl
+snap install spotify
+snap install ufw
+snap install vlc
+snap install syncthing
+snap install htop
+snap install gnome-system-monitor
+snap install discord
+snap install wire
+snap install code --classic
+
