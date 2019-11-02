9ce8c32e7dea127cd0dafd2247b0650b72cb7d4e
redjax <jackenyon@gmail.com>
2019-11-02 14:02:36 -0400

    Broke the script up into functions

--- Fedora/fedora-setup.sh
+++ Fedora/fedora-setup.sh
@@ -5,7 +5,7 @@
 
 # Create variables
 gitcl='${gitcl} --verbose'
-this_dir="$(dirname "$(readlink -f "$0")")"
+this_dir="$(dirname "$(readlink -f "$0")")"  # Get path to script in case it's not in my regular spot
 app_installs="$this_dir/app-installs"
 app_install_scripts="$app_installs/app-install-scripts"
 crontasks_dir="$this_dir/crontasks"
@@ -15,6 +15,8 @@ crontasks_dir="$this_dir/crontasks"
 function install_dnf_apps () {
     # Install apps via dnf if no app-install script exists
     apps="git curl python3 python3-pip tmux terminator tilix alacarte android-tools"
+
+    echo -e "\n Installing $apps\n"
     sudo dnf install -y $apps
 }
 
@@ -28,6 +30,7 @@ function install_apps () {
 
 function install_nvim () {
     # Install nvim
+    echo -e "\nRunning nvim install script\n"
     nvim_installer="$app_installs/nvim-setup.sh"
     exec $nvim_installer
 }
@@ -36,8 +39,8 @@ function create_cron_jobs () {
     # Move cron jobs into cron dir & create cron jobs
 
     # Create folder in /opt and copy scripts to it
-    $(mkdir /opt/backup-scripts)
-    cp -R "$crontasks_dir/backup-scripts/*" /opt/backup-scripts
+    exec $(mkdir /opt/backup-scripts)
+    exec $(cp -R "$crontasks_dir/backup-scripts/*" /opt/backup-scripts)
 
     # Create cron job for backing up Gnome
     echo "0 0 */3 * * /opt/backup-scripts/backup-gnome.sh" | crontab -
@@ -46,11 +49,21 @@ function create_cron_jobs () {
     # ./opt/backup-scripts/restore-gnome.sh
 }
 
-# Install dnf apps first
-install_dnf_apps
+function main () {
+    # Main script to run functions
 
-# Run nvim install script
-install_nvim
+    clear
+    # Install dnf apps first
+    install_dnf_apps
+
+    # Run nvim install script
+    # install_nvim
+
+    # Run the app install scripts in app-install-scripts
+    # install_apps
+}
+
+main
 
 # Themes, Fonts, and Icons
 
