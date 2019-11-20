e8ebfd314d08ef466fd5a94740c8b02911534683
redjax <jackenyon@gmail.com>
2019-11-20 00:24:12 -0500

    Moved nvim to dots directory, since the install process is the same across distributions.
    This way, I can run all of my dot symlinks from one directory across distros.

---
+++ dots/create-syms.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/sh
+
+# Create symlinks for dots
+
+# Starting out, this script will just find and run scripts.
+# Eventually, I'll consolidate them into this one script.
+
+# Symlink bash dots
+this_dir="$(dirname "$(readlink -f "$0")")"
+
+. $this_dir/symlink-bashdots.sh
+. $this_dir/home/config/sym-nvim.sh
