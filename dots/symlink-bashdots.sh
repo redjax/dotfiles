f9fa9db1fbd70c41622bc4d914b8d6ad077f3ee5
redjax <jackenyon@gmail.com>
2019-11-25 12:34:35 -0500

    Added code for symlinking .tmux/ and .tmux.conf

--- dots/symlink-bashdots.sh
+++ dots/symlink-bashdots.sh
@@ -23,6 +23,8 @@ create_sym () {
 create_backup $HOME/.bashrc
 create_backup $HOME/.bash_aliases
 create_backup $HOME/.bash_profile
+create_backup $HOME/.tmux
+create_backup $HOME/.tmux.conf
 
 # Create symlinks
 create_sym .bashrc
@@ -30,6 +32,10 @@ cd $this_dir
 create_sym .bash_aliases
 cd $this_dir
 create_sym .bash_profile
+cd $this_dir
+create_sym .tmux.conf
+cd $this_dir
+create_sym .tmux/
 
 # Source new bashrc
 if [[ -f $HOME/.bashrc ]]; then
