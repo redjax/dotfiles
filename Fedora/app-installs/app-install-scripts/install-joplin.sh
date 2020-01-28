2f5ffb14566bbc3b739aa1b5b76f960148eb2504
redjax <jackenyon@gmail.com>
2020-01-28 21:37:21 +0000

    Add joplin install script to Fedora & Ubuntu (server) dirs

---
+++ Fedora/app-installs/app-install-scripts/install-joplin.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+#
+# Script to install Joplin & create desktop icon
+#
+# Works on Fedora/Ubuntu officially, according to Joplin's homepage
+
+cd $HOME/Downloads
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
