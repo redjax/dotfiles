5629cf4457f53731f86e7eb184f147b7e985f6e0
redjax <5534031+redjax@users.noreply.github.com>
2025-07-18 14:39:00 -0400

    fix(flatpak): Handle Rio installed with Flatpak

---
+++ home/dot_chezmoi_scripts/run_rio-flatpak-config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Detect if flatpak is installed
+if command -v flatpak &>/dev/null; then
+
+  ## Detect if Rio is installed as a flatpak
+  if flatpak info com.rioterm.Rio &>/dev/null; then
+    echo "Running Rio Flatpak config copy script..."
+    
+    SRC="$HOME/.config/rio"
+    DEST="$HOME/.var/app/com.rioterm.Rio/config/rio"
+
+    if [ -d "$SRC" ]; then
+      mkdir -p "$DEST"
+      cp -a "$SRC/." "$DEST/"
+      echo "Copied Rio config from '$SRC' to '$DEST'"
+    else
+      echo "No config found at '$SRC'."
+    fi
+
+  else
+    echo "Rio Flatpak is not installed."
+  fi
+
+else
+  echo "Flatpak is not installed."
+fi
+
