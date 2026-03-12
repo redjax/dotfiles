0bc5b7bc2e5dde6863487ff40b23bf733a242852
redjax <5534031+redjax@users.noreply.github.com>
2026-03-12 02:24:43 -0400

    script: Set env vars

--- scripts/docker/test-platforms.sh
+++ scripts/docker/test-platforms.sh
@@ -11,3 +11,15 @@ declare -a IMAGES=(
   fedora:43
   archlinux:latest
 )
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT=$(realpath -m "$THIS_DIR/../..")
+CONTAINER_DIR="${REPO_ROOT}/.containers/render"
+ORIGINAL_PATH="$(pwd)"
+
+GITHUB_USERNAME="${GITHUB_USERNAME:-}"
+HOST_USER="${HOST_USER:-}"
+
+## Arrays to store pass/fail jobs
+declare -a ARR_PASSED=()
+declare -a ARR_FAILED=()
